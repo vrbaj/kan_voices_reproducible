cb14b8a7101c96ae615c8c51fefa4580a91ca7f0
vrbaj <vrbik.vrbik@gmail.com>
2024-07-29 17:34:34 +0900

    CPU

--- kan_arch_mc_023.py
+++ kan_arch_mc_023.py
@@ -95,7 +95,7 @@ torch.set_default_dtype(torch.float64)
 # path to training datasets
 datasets = Path("", "training_data", "men")
 # select computational device -> changed to CPU as it is faster for small datasets (as SVD)
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DEVICE = "cpu" # torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #torch.set_default_device(DEVICE)
 print(f"The {DEVICE} will be used for the computation..")
 for dataset in datasets.iterdir():
