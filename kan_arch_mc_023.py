5b6a08d328002e475805857e25810b7dc0344292
vrbaj <vrbik.vrbik@gmail.com>
2024-08-02 13:55:09 +0900

    kan_arch_mc_023 deterministic stuff added

--- kan_arch_mc_023.py
+++ kan_arch_mc_023.py
@@ -28,7 +28,7 @@ def set_seed(seed):
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    torch.use_deterministic_algorithms(True)
+    #torch.use_deterministic_algorithms(True)
 
 
 
