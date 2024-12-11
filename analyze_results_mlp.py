8c74f90e01dfa12f322a62e13541b7981a722475
jirsat <45206873+jirsat@users.noreply.github.com>
2024-12-11 11:25:55 +0100

    analyze results for mlp

--- analyze_results_mlp.py
+++ analyze_results_mlp.py
@@ -4,7 +4,9 @@ Script to analyze results of kan_arch_mc.py.
 import pickle
 from pathlib import Path
 import numpy as np
+from tqdm import tqdm
 
+np.seterr(all="raise")
 
 def main():
     """
@@ -30,56 +32,51 @@ def main():
         }
     }
 
-    pickled_results_path = Path(".", "results_mlp", "training_data")
+    pickled_results_path = Path(".", "results_mlp")
     for dataset in sorted(pickled_results_path.iterdir()):
         sex = dataset.name
-        for arch_result in dataset.iterdir():
-            best_metrics = {"mcc": [], "sensitivity": [], "specificity": [], "gm": [], "uar": [], "bm": []}
+        print("Computing results for", sex)
+        best_uar = 0
+        for arch_result in tqdm(list(dataset.iterdir())):
+            result_all_splits = {}
             for result in arch_result.glob("*.pickle"):
                 # read result for a single train/test split
+                # and save it into dictionary
+                idx = int(result.stem.split("_")[-1])
                 with open(result, "rb") as f:
-                    experiment_results = pickle.load(f)
-                # compute UAR
-                is_nan = False
-                try:
-                    mcc = [(tp * tn - fp * fn) / np.sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn))
-                            for tp, tn, fp, fn
-                            in zip(experiment_results["test_tp"],
-                                   experiment_results["test_tn"],
-                                   experiment_results["test_fp"],
-                                   experiment_results["test_fn"])]
-                    best_idx = np.argmax(mcc)
-                    sensitivity = experiment_results["tp"][best_idx] / (experiment_results["tp"][best_idx] + experiment_results["fn"][best_idx])
-                    specificity = experiment_results["tn"][best_idx] / (experiment_results["tn"][best_idx] + experiment_results["fp"][best_idx])
+                    result_all_splits[idx] = pickle.load(f)
 
-                    best_metrics["mcc"].append(np.max(mcc))
-                    best_metrics["sensitivity"].append(sensitivity)
-                    best_metrics["specificity"].append(specificity)
-                    best_metrics["gm"].append(np.sqrt(sensitivity * specificity))
-                    best_metrics["bm"].append(sensitivity + specificity - 1)
-                    best_metrics["uar"].append((sensitivity + specificity) / 2)
+            uars = np.array([result["uar"] for result in result_all_splits.values()])
+            best_idx = np.argmax(np.mean(uars,axis=0))
+            tps = np.array([result["tp"][best_idx] for result in result_all_splits.values()])
+            fps = np.array([result["fp"][best_idx] for result in result_all_splits.values()])
+            tns = np.array([result["tn"][best_idx] for result in result_all_splits.values()])
+            fns = np.array([result["fn"][best_idx] for result in result_all_splits.values()])
 
-                except KeyError:
-                    print(experiment_results.keys())
-                    print(f"error in {result.resolve()}")
-                except ZeroDivisionError:
-                    is_nan = True
+            sensitivity = tps/(tps+fns)
+            specificity = tns/(tns+fps)
+            try:
+                mcc = (tps * tns - fps * fns) / np.sqrt((tps + fps) * (tps + fns) * (tns + fps) * (tns + fns))
+            except FloatingPointError:
+                # Zero division
+                mcc = -1*np.ones_like(specificity)
 
-            if not is_nan and np.mean(best_metrics["mcc"]) > best_results[sex]["mcc"]:
-                    best_results[sex]["mcc"] = np.mean(best_metrics["mcc"])
-                    best_results[sex]["mcc_std"] = np.std(best_metrics["mcc"])
-                    best_results[sex]["sensitivity"] = np.mean(best_metrics["sensitivity"])
-                    best_results[sex]["sensitivity_std"] = np.std(best_metrics["sensitivity"])
-                    best_results[sex]["specificity"] = np.mean(best_metrics["specificity"])
-                    best_results[sex]["specificity_std"] = np.std(best_metrics["specificity"])
-                    best_results[sex]["bm"] = np.mean(best_metrics["bm"])
-                    best_results[sex]["bm_std"] = np.std(best_metrics["bm"])
-                    best_results[sex]["gm"] = np.mean(best_metrics["gm"])
-                    best_results[sex]["gm_std"] = np.std(best_metrics["gm"])
-                    best_results[sex]["uar"] = np.mean(best_metrics["uar"])
-                    best_results[sex]["uar_std"] = np.std(best_metrics["uar"])
-                    best_results[sex]["architecture"] = arch_result.name
-                    best_results[sex]["settings"] = kan_settings.name
+
+            if np.mean(sensitivity/2+specificity/2) > best_uar:
+                best_uar = np.mean(sensitivity/2+specificity/2)
+                best_results[sex]["mcc"] = np.mean(mcc)
+                best_results[sex]["mcc_std"] = np.std(mcc)
+                best_results[sex]["sensitivity"] = np.mean(sensitivity)
+                best_results[sex]["sensitivity_std"] = np.std(sensitivity)
+                best_results[sex]["specificity"] = np.mean(specificity)
+                best_results[sex]["specificity_std"] = np.std(specificity)
+                best_results[sex]["bm"] = np.mean(sensitivity + specificity - 1)
+                best_results[sex]["bm_std"] = np.std(sensitivity + specificity - 1)
+                best_results[sex]["gm"] = np.mean(np.sqrt(sensitivity * specificity))
+                best_results[sex]["gm_std"] = np.std(np.sqrt(sensitivity * specificity))
+                best_results[sex]["uar"] = np.mean(sensitivity/2+specificity/2)
+                best_results[sex]["uar_std"] = np.std(sensitivity/2+specificity/2)
+                best_results[sex]["architecture"] = arch_result.name
     return best_results
 
 if __name__ == "__main__":
@@ -88,3 +85,4 @@ if __name__ == "__main__":
     print(f"BEST MEN: {best_results_dict["men"]["uar"]}")
     print(f"BEST WOMEN DICT: {best_results_dict["women"]}")
     print(f"BEST MEN DICT: {best_results_dict["men"]}")
+    print(best_results_dict)
