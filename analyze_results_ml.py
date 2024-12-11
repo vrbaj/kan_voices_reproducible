ed27e3268eec422d57ecc9e3dc91bffdfac69445
jirsat <45206873+jirsat@users.noreply.github.com>
2024-12-11 09:21:07 +0100

    analyze results ml

--- analyze_results_ml.py
+++ analyze_results_ml.py
@@ -6,18 +6,18 @@ import pandas as pd
 if __name__ == "__main__":
     sexes = ["women", "men"]
     results_path = Path(".", "results")
-    classifiers = [item.name for item in results_path.iterdir()]
+    classifiers = [item.name for item in results_path.glob("*/*")]
     results = {"men": [],
                "women": []}
     cls_params = {}
     for classifier in classifiers:
         for sex in sexes:
-            to_process = pd.read_csv(results_path.joinpath(classifier, sex, "0", "results.csv"))
+            to_process = pd.read_csv(results_path.joinpath(sex, classifier, "results.csv"))
 
             best = to_process[to_process.mean_test_mcc == to_process.mean_test_mcc.max()].iloc[0]
             cls_params[f"{classifier}_{sex}"] = best.params
             best["classifier"] = classifier
-            best = best.drop(["params", "mean_test_accuracy", "std_test_accuracy"])
+            best = best.drop(["params", "mean_test_accuracy"])
             results[sex].append(best)
 
     print(pd.DataFrame(results["men"]).reset_index(drop=True))
@@ -25,3 +25,4 @@ if __name__ == "__main__":
     print(cls_params)
 
     pd.DataFrame(results["men"]).reset_index(drop=True).reset_index(drop=True).transpose().to_latex("ml_results_men.tex")
+    pd.DataFrame(results["women"]).reset_index(drop=True).reset_index(drop=True).transpose().to_latex("ml_results_women.tex")
