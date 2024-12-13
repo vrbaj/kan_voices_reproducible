515f9b02698ae74bcb3552334284532f66e43379
jirsat <45206873+jirsat@users.noreply.github.com>
2024-12-13 15:54:21 +0100

    add analyzing kans

--- analyze_results_kan.py
+++ analyze_results_kan.py
@@ -1,9 +1,13 @@
 """
-Script to analyze results of kan_arch_mc.py.
+Script to analyze results of kan_pipeline.py.
 """
 import pickle
 from pathlib import Path
 import numpy as np
+from tqdm import tqdm
+
+
+np.seterr(all="raise")
 
 
 def main():
@@ -29,58 +33,65 @@ def main():
             "bm": 0.0
         }
     }
+    optimistic_uar = {
+        "women": 0.0,
+        "men": 0.0,
+    }
 
     pickled_results_path = Path(".", "results_kan")
     for kan_settings in sorted(pickled_results_path.iterdir()):
         print(f"evaluating {kan_settings}")
-        for dataset in sorted(kan_settings.joinpath("training_data").iterdir()):
+        for dataset in sorted(kan_settings.iterdir()):
             sex = dataset.name
-            for arch_result in dataset.iterdir():
-                best_metrics = {"mcc": [], "sensitivity": [], "specificity": [], "gm": [], "uar": [], "bm": []}
+            print("evaluating", sex)
+            assert sex in best_results
+            trash = 0
+            for arch_result in tqdm(list(dataset.iterdir())):
+                result_all_splits = {}
                 for result in arch_result.glob("*.pickle"):
                     # read result for a single train/test split
+                    # and save it into dictionary
+                    idx = int(result.stem.split("_")[-1])
                     with open(result, "rb") as f:
-                        experiment_results = pickle.load(f)
-                    try:
-                        mcc = [(tp * tn - fp * fn) / np.sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn))
-                            for tp, tn, fp, fn
-                            in zip(experiment_results["test_tp"],
-                                   experiment_results["test_tn"],
-                                   experiment_results["test_fp"],
-                                   experiment_results["test_fn"])]
-                        best_idx = np.argmax(mcc)
-                        sensitivity = experiment_results["test_tp"][best_idx] / (
-                                    experiment_results["test_tp"][best_idx] + experiment_results["test_fn"][best_idx])
-                        specificity = experiment_results["test_tn"][best_idx] / (
-                                    experiment_results["test_tn"][best_idx] + experiment_results["test_fp"][best_idx])
+                        result_all_splits[idx] = pickle.load(f)
 
-                        best_metrics["mcc"].append(np.max(mcc))
-                        best_metrics["sensitivity"].append(sensitivity)
-                        best_metrics["specificity"].append(specificity)
-                        best_metrics["gm"].append(np.sqrt(sensitivity * specificity))
-                        best_metrics["bm"].append(sensitivity + specificity - 1)
-                        best_metrics["uar"].append((sensitivity + specificity) / 2)
+                uars = np.array([result["test_uar"] for result in result_all_splits.values()])
+                optimistic_uar[sex] = np.max([optimistic_uar[sex], np.mean(np.max(uars,axis=1))])
+                if np.any(np.all(uars <= 0.5,axis=1)):
+                    trash += 1
+                best_idx = np.argmax(np.mean(uars,axis=0))
+                tps = np.array([result["test_uar"][best_idx] for result in result_all_splits.values()])
+                fps = np.array([result["test_uar"][best_idx] for result in result_all_splits.values()])
+                tns = np.array([result["test_uar"][best_idx] for result in result_all_splits.values()])
+                fns = np.array([result["test_uar"][best_idx] for result in result_all_splits.values()])
 
-                    except KeyError:
-                        print(experiment_results.keys())
-                        print(f"error in {result.resolve()}")
+                sensitivity = tps/(tps+fns)
+                specificity = tns/(tns+fps)
+                try:
+                    mcc = (tps * tns - fps * fns) / np.sqrt((tps + fps) * (tps + fns) * (tns + fps) * (tns + fns))
+                except FloatingPointError:
+                    # Zero division
+                    mcc = -1*np.ones_like(specificity)
 
-                if np.mean(best_metrics["mcc"]) > best_results[sex]["mcc"]:
-                    best_results[sex]["mcc"] = np.mean(best_metrics["mcc"])
-                    best_results[sex]["mcc_std"] = np.std(best_metrics["mcc"])
-                    best_results[sex]["sensitivity"] = np.mean(best_metrics["sensitivity"])
-                    best_results[sex]["sensitivity_std"] = np.std(best_metrics["sensitivity"])
-                    best_results[sex]["specificity"] = np.mean(best_metrics["specificity"])
-                    best_results[sex]["specificity_std"] = np.std(best_metrics["specificity"])
-                    best_results[sex]["bm"] = np.mean(best_metrics["bm"])
-                    best_results[sex]["bm_std"] = np.std(best_metrics["bm"])
-                    best_results[sex]["gm"] = np.mean(best_metrics["gm"])
-                    best_results[sex]["gm_std"] = np.std(best_metrics["gm"])
-                    best_results[sex]["uar"] = np.mean(best_metrics["uar"])
-                    best_results[sex]["uar_std"] = np.std(best_metrics["uar"])
+
+                if np.mean(sensitivity/2+specificity/2) > best_results[sex]["uar"]:
+                    best_results[sex]["mcc"] = np.mean(mcc)
+                    best_results[sex]["mcc_std"] = np.std(mcc)
+                    best_results[sex]["sensitivity"] = np.mean(sensitivity)
+                    best_results[sex]["sensitivity_std"] = np.std(sensitivity)
+                    best_results[sex]["specificity"] = np.mean(specificity)
+                    best_results[sex]["specificity_std"] = np.std(specificity)
+                    best_results[sex]["bm"] = np.mean(sensitivity + specificity - 1)
+                    best_results[sex]["bm_std"] = np.std(sensitivity + specificity - 1)
+                    best_results[sex]["gm"] = np.mean(np.sqrt(sensitivity * specificity))
+                    best_results[sex]["gm_std"] = np.std(np.sqrt(sensitivity * specificity))
+                    best_results[sex]["uar"] = np.mean(sensitivity/2+specificity/2)
+                    best_results[sex]["uar_std"] = np.std(sensitivity/2+specificity/2)
                     best_results[sex]["architecture"] = arch_result.name
-                    best_results[sex]["settings"] = kan_settings.name
+            print("Trash: ", trash)
+    print(f"Optimistic UAR: {optimistic_uar}")
     return best_results
+
 
 
 if __name__ == "__main__":
