62da1053f85837eaf45fb030cc40644c7433b6a8
vrbaj <Jan.Vrba@vscht.cz>
2025-02-24 12:17:48 +0100

    I have no idea what I did. Sorry for that.

---
+++ adam_search_results.py
@@ -0,0 +1,48 @@
+import pickle
+from pathlib import Path
+import numpy as np
+import tqdm
+from matplotlib import pyplot as plt
+
+best_uar = 0
+actual_uar = 10 * [0]
+best_arch = ""
+sex = "men"
+for params_setting in tqdm.tqdm(Path("results_kan_adam").iterdir()):
+    print(params_setting.name)
+    try:
+        for arch in params_setting.joinpath(sex).iterdir():
+            for idx, split_fold in enumerate(arch.iterdir()):
+                data = pickle.load(open(str(split_fold), "rb"))
+                actual_uar[idx] = np.max(data["test_uar"])
+                #actual_uar[idx] = data["test_uar"][0]
+            if np.mean(actual_uar) >= best_uar:
+                best_uar = np.mean(actual_uar)
+                best_settings = params_setting.name
+                best_arch = arch.name
+    except FileNotFoundError:
+        pass
+print(f"best settings {best_settings}:best arch {best_arch} - uar: {best_uar}")
+
+loss_path = Path("results_kan_adam", best_settings, sex, best_arch)
+plt.figure(figsize=(10, 6))
+for idx, file_path in enumerate(loss_path.glob('*.pickle')):
+    #print(f'opening {file_path.name}')
+
+    with open(file_path, 'rb') as f:
+        data = pickle.load(f)
+    loss_values = data['test_loss']
+    val_loss_values = data['test_uar']
+    # Plot loss function values
+    plt.plot(list(range(1, 201)), val_loss_values, label=f'Loss cross-val split {idx + 1}')
+
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.title('Loss Function Over Epochs')
+plt.legend()
+plt.grid()
+plt.tight_layout()
+plt.xticks([1] + list(range(10, 101, 10)))
+plt.xlim([1, 200])
+# plt.savefig('kan_women_train.pdf', dpi=300, format='pdf')
+plt.show()
--- scp_download_blade.sh
+++ scp_download_blade.sh
@@ -1,4 +1,1 @@
-SET source=/disk2/vrbaj/kan_voices_reproducible/results_2layer*_100epochs
-SET dsc=/home/vrbaj/tmp
-
-scp -r vrbaj@446-a336-j1.vscht.cz:/disk2/vrbaj/kan_voices_reproducible/results_mlp /home/vrbaj/tmp
+scp -r vrbaj@446-a336-j4.vscht.cz:/disk2/vrbaj/kan_voices_reproducible/training_data /home/vrbaj/tmp
